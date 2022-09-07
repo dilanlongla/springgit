481290548b785e506ca4776231136bf91424a29f
dilanlongla <dilanlongla@yahoo.com>
2022-09-07 10:21:44 +0100

    another one

--- src/main/java/com/dilanlongla/code/StarterappApplication.java
+++ src/main/java/com/dilanlongla/code/StarterappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StarterappApplication {
 
 	public static void main(String[] args) {
+		//some comment
 		SpringApplication.run(StarterappApplication.class, args);
 	}
 
